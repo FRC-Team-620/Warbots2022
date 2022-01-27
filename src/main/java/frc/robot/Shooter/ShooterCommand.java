965ddbe5ce8bd8c5969c39f4212809d9df705226
tjamesw123 <tweinhardt@verizon.net>
2022-01-27 18:19:59 -0500

    Read shooter RPM to SmartDashboard

--- src/main/java/frc/robot/Shooter/ShooterCommand.java
+++ src/main/java/frc/robot/Shooter/ShooterCommand.java
@@ -1,12 +1,14 @@
 package frc.robot.Shooter;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ShooterCommand extends CommandBase {
     protected XboxController driverXbox;
     protected ShooterSubsystem shooterSubsystem;
-
+    protected double bangBangTolerance = 0.05, intermittantSpeed = 0.3, minRPM = 1000, maxRPM = 3000;
+    
     public ShooterCommand(ShooterSubsystem shooterSubsystem, XboxController driverXbox) {
         addRequirements(shooterSubsystem);
         this.shooterSubsystem = shooterSubsystem;
@@ -15,9 +17,17 @@ public class ShooterCommand extends CommandBase {
 
     @Override
     public void execute() {
-        double speed = driverXbox.getRightY();
-        //double speed = 0.5;
+        double speed = driverXbox.getRightTriggerAxis();
         shooterSubsystem.setShooterSpeed(speed);
+        // double rpm = driverXbox.getRightY() * (maxRPM - minRPM) + minRPM;
+        // if (shooterSubsystem.getRPM() < rpm - bangBangTolerance * rpm)
+        //     shooterSubsystem.setShooterSpeed(intermittantSpeed);
+        // else
+        //     shooterSubsystem.setShooterSpeed(0);
+
+        // System.out.println(shooterSubsystem.getRPM());
+
+        SmartDashboard.putNumber("Shooter RPM: ", shooterSubsystem.getRPM());
     }
 
     @Override
--- src/main/java/frc/robot/Shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/Shooter/ShooterSubsystem.java
@@ -3,26 +3,35 @@ package frc.robot.Shooter;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
     protected final CANSparkMax leftShooterMotor, rightShooterMotor;
+    protected final RelativeEncoder encoder;
+
     public ShooterSubsystem() {
         leftShooterMotor = new CANSparkMax(5, MotorType.kBrushless);
         rightShooterMotor = new CANSparkMax(7, MotorType.kBrushless);
+        encoder = rightShooterMotor.getEncoder();
 
         leftShooterMotor.restoreFactoryDefaults();
         rightShooterMotor.restoreFactoryDefaults();
 
-        IdleMode mode = IdleMode.kCoast; //brakes
+        IdleMode mode = IdleMode.kCoast; // brakes
         leftShooterMotor.setIdleMode(mode);
         rightShooterMotor.setIdleMode(mode);
 
         leftShooterMotor.follow(rightShooterMotor, true);
-        //leftShooterMotor.setInverted(false);
-        //rightShooterMotor.setInverted(false);
+        // leftShooterMotor.setInverted(false);
+        // rightShooterMotor.setInverted(false);
 
+    }
+
+    public double getRPM() {
+        return encoder.getPosition() / encoder.getCountsPerRevolution() * Constants.shooterGearRatio;
     }
 
     public void setShooterSpeed(double speed) {
