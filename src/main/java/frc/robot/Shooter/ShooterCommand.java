54b15aaaee2e72a9e98c5e79be11846ef08fc14c
EventHorizon150 <91739640+EventHorizon150@users.noreply.github.com>
2022-02-02 20:07:24 -0500

    Add support for setting the RPM of the shooter and setting the step by which the RPM display is rounded

--- src/main/java/frc/robot/Shooter/ShooterCommand.java
+++ src/main/java/frc/robot/Shooter/ShooterCommand.java
@@ -13,13 +13,16 @@ public class ShooterCommand extends CommandBase {
     // protected long prevRotations = 0;
     // protected double secondsTimestep = 0.2, prevTime, currentTime, prevRPM = 0;
 
-    protected double bangBangTolerance = 0.05, intermittantAcceleration = 0.01, 
-        intermittantDeceleration = 0.03, minRPM = 1000, maxRPM = 5000, currentSpeed = 0;
+    protected double bangBangTolerance = 0.01, intermittantAcceleration = 0.001, 
+        intermittantDeceleration = 0.001, minRPM = 0, maxRPM = 5000, currentSpeed = 0;
 
     public ShooterCommand(ShooterSubsystem shooterSubsystem, XboxController driverXbox) {
 
         // timer.start();
         // prevTime = timer.get();
+
+        SmartDashboard.putNumber("Round to: ", 50);
+        SmartDashboard.putNumber("Set RPM: ", 0);
 
         addRequirements(shooterSubsystem);
         this.shooterSubsystem = shooterSubsystem;
@@ -30,31 +33,26 @@ public class ShooterCommand extends CommandBase {
     public void execute() {
         // double speed = driverXbox.getRightTriggerAxis();
         // shooterSubsystem.setShooterSpeed(speed);
-        double rpm = driverXbox.getRightY() * (maxRPM - minRPM) + minRPM;
-        rpm = roundUpToNearestMultiple(rpm, 50);
-        if (shooterSubsystem.getRPM() < rpm - bangBangTolerance * rpm)
-            setShooterSpeedAndUpdate(currentSpeed + intermittantAcceleration);
+        double roundTo = SmartDashboard.getNumber("Round to: ", 50);
+        double input = driverXbox.getRightTriggerAxis(), rpm;
+        if(input > 0)
+            rpm = input * (maxRPM - minRPM) + minRPM;
         else
+            rpm = SmartDashboard.getNumber("Set RPM: ", 0);
+        double currRPM = shooterSubsystem.getRPM();
+        SmartDashboard.putNumber("Input RPM: ", rpm);
+        rpm = roundUpToNearestMultiple(rpm, 50);
+        if (currRPM < rpm - bangBangTolerance * rpm)
+            setShooterSpeedAndUpdate(currentSpeed + intermittantAcceleration);
+        else if (currRPM > rpm + bangBangTolerance * rpm)
             setShooterSpeedAndUpdate(currentSpeed - intermittantDeceleration);
 
-        // // System.out.println(shooterSubsystem.getRPM());
-
-        // SmartDashboard.putNumber("Shooter RPM: ", shooterSubsystem.getRPM());
-
-        // currentTime = timer.get(); // milliseconds to seconds
-
-        // System.out.println(shooterSubsystem.getRPM());
-
-        // if(currentTime - prevTime >= secondsTimestep) {
-        // prevTime = currentTime;
-        // prevRPM = shooterSubsystem.getRPM(prevRotations, secondsTimestep);
-        // prevRotations = shooterSubsystem.getTotalWheelRotations();
-        // }
-        SmartDashboard.putNumber("Shooter RPM: ", shooterSubsystem.getRPM());
+        SmartDashboard.putNumber("Shooter RPM: ", 
+            roundUpToNearestMultiple(shooterSubsystem.getRPM(), (int)roundTo));
     }
 
-    static long roundUpToNearestMultiple(double input, long step) {
-        long i = (long) Math.ceil(input);
+    static long roundUpToNearestMultiple(double input, int step) {
+        int i = (int) Math.ceil(input);
         return ((i + step - 1) / step) * step;
     }
 
