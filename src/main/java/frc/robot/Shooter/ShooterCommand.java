8d061acdc0b24ca987a69d16f2a7a96d106f550d
EventHorizon150 <91739640+EventHorizon150@users.noreply.github.com>
2022-02-07 08:46:07 -0500

    Change proportionality constant 'diffConst' to scientific notation and change SparkMax indeces to make more sense (7 -> 6, we'll flash them later)

--- src/main/java/frc/robot/Shooter/ShooterCommand.java
+++ src/main/java/frc/robot/Shooter/ShooterCommand.java
@@ -7,31 +7,33 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class ShooterCommand extends CommandBase {
     protected XboxController driverXbox;
     protected ShooterSubsystem shooterSubsystem;
-    //protected double bangBangTolerance = 0.05, intermittantSpeed = 0.3, minRPM = 1000, maxRPM = 3000;
-    
+    // protected double bangBangTolerance = 0.05, intermittantSpeed = 0.3, minRPM =
+    // 1000, maxRPM = 3000;
+
     // protected Timer timer = new Timer(); // For dealing with RPM
     // protected long prevRotations = 0;
     // protected double secondsTimestep = 0.2, prevTime, currentTime, prevRPM = 0;
 
-	protected NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    protected NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
     protected NetworkTableEntry entryHasTarget = table.getEntry("tv");
     protected NetworkTableEntry entryX = table.getEntry("tx");
     protected NetworkTableEntry entryY = table.getEntry("ty");
     protected NetworkTableEntry entryArea = table.getEntry("ta");
 
-    protected double bangBangTolerance = 0.01, minRPM = 0, maxRPM = 5500, 
-        currentSpeed = 0, diffConst = 0.000006, acceleration, rpm, input, 
-        roundTo, currRPM;
+    protected double bangBangTolerance = 0.01, minRPM = 0, maxRPM = 5500,
+            currentSpeed = 0, diffConst = 6 * Math.pow(10, -6), acceleration, 
+            rpm, input, roundTo, currRPM;
 
     public ShooterCommand(ShooterSubsystem shooterSubsystem, XboxController driverXbox) {
-		// timer.start();
+        // timer.start();
         // prevTime = timer.get();
 
         SmartDashboard.putNumber("Round to: ", 5);
-        SmartDashboard.putNumber("Set RPM: ", 0);        
+        SmartDashboard.putNumber("Set RPM: ", 0);
         addRequirements(shooterSubsystem);
         this.shooterSubsystem = shooterSubsystem;
         this.driverXbox = driverXbox;
@@ -39,7 +41,7 @@ public class ShooterCommand extends CommandBase {
 
     @Override
     public void execute() {
-    	boolean hasTarget = entryHasTarget.getDouble(0.0) == 1.0;
+        boolean hasTarget = entryHasTarget.getDouble(0.0) == 1.0;
         double x = entryX.getDouble(0.0);
         double y = entryY.getDouble(0.0);
         double area = entryArea.getDouble(0.0);
@@ -47,7 +49,7 @@ public class ShooterCommand extends CommandBase {
         input = driverXbox.getRightTriggerAxis();
         roundTo = SmartDashboard.getNumber("Round to: ", 5);
         currRPM = shooterSubsystem.getRPM();
-        
+
         // post to smart dashboard periodically
         SmartDashboard.putBoolean("LimelightHasTarget", hasTarget);
         SmartDashboard.putNumber("LimelightX", x);
@@ -58,15 +60,15 @@ public class ShooterCommand extends CommandBase {
         if (hasTarget && area > 0.05) {
             // call motor to rotate shooter to optimal position
         }
-		// double speed = driverXbox.getRightTriggerAxis();
+        // double speed = driverXbox.getRightTriggerAxis();
         // shooterSubsystem.setShooterSpeed(speed);
-        if(input > 0)
+        if (input > 0)
             rpm = input * (maxRPM - minRPM) + minRPM;
         else {
             rpm = SmartDashboard.getNumber("Set RPM: ", 0);
-            if(rpm > maxRPM)
+            if (rpm > maxRPM)
                 rpm = maxRPM;
-            else if(rpm < minRPM)
+            else if (rpm < minRPM)
                 rpm = minRPM;
         }
         acceleration = diffConst * (rpm - currRPM);
@@ -74,9 +76,10 @@ public class ShooterCommand extends CommandBase {
         // if(Math.abs(rpm-currRPM) > (bangBangTolerance * rpm))
         setShooterSpeedAndUpdate(currentSpeed + acceleration);
 
-        SmartDashboard.putNumber("Shooter RPM: ", 
-            roundUpToNearestMultiple(currRPM, (int)roundTo));
+        SmartDashboard.putNumber("Shooter RPM: ",
+                roundUpToNearestMultiple(currRPM, (int) roundTo));
     }
+
     static long roundUpToNearestMultiple(double input, int step) {
         int i = (int) Math.ceil(input);
         return ((i + step - 1) / step) * step;
