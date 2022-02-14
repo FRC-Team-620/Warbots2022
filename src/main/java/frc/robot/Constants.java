4ac93080357a70c1d34e95967a8665d9cce408a3
tjamesw123 <94910672+tjamesw123@users.noreply.github.com>
2022-02-14 18:15:45 -0500

    Updated pathing, Setup Presets, Updated Constants.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -47,7 +47,7 @@ public final class Constants {
     public static final double kEncoderDistancePerPulse = 0.0359;//0.00136 
 
     public static final double speed = 1.0;
-    public static final double rotation = -0.5;
+    public static final double rotation = -0.75;
     public static final double rampRate = 0.0;
 
     public static final double limelightHeight = 0.5588; //based on last year's robot
