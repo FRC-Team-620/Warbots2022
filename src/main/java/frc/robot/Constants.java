8ffc9a7e06ff71d184d43590219aa6063d6cb853
EventHorizon150 <91739640+EventHorizon150@users.noreply.github.com>
2022-03-09 18:24:17 -0500

    Woops haha

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -114,6 +114,8 @@ public final class Constants {
     public static final double diffConstShooter = 6 * Math.pow(10, -6);
     public static final double diffConstWinchHold = 0.00001;
     public static final double diffConstKeepPosition = 0.00001; // TODO: Test this constant and optimize it
+    
+    public static final int holdTime = 100;
 
 
     public static final double leftBias = 1.3;
