519316bde9971fda3f6f4c59d55af1c67958cbae
tjamesw123 <94910672+tjamesw123@users.noreply.github.com>
2022-03-21 10:58:07 -0400

    Fixed OuttakeBall command

--- src/main/java/frc/robot/Loader/OuttakeBall.java
+++ src/main/java/frc/robot/Loader/OuttakeBall.java
@@ -2,8 +2,11 @@ package frc.robot.Loader;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.InnerIntake.InnerIntake;
+import frc.robot.InnerIntake.ReverseSpinInnerIntake;
+import frc.robot.IntakeArms.DeactivateIntakeArmsMotor;
 import frc.robot.IntakeArms.IntakeArms;
 import frc.robot.IntakeArms.IntakeArmsMotor;
+import frc.robot.IntakeArms.RetractIntakeArms;
 
 public class OuttakeBall extends ParallelCommandGroup {
     private InnerIntake innerIntake;
@@ -17,7 +20,7 @@ public class OuttakeBall extends ParallelCommandGroup {
         addCommands(
             new ReverseSpinInnerIntake(innerIntake),
             new RetractIntakeArms(intakeArms),
-            new SpinIntakeArmsMotor(intakeArmsMotor)
+            new DeactivateIntakeArmsMotor(intakeArmsMotor)
         );
     }
 }
