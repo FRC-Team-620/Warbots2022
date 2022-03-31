1ac383658f795b0151f0cd3120d765c56331d697
EventHorizon150 <91739640+EventHorizon150@users.noreply.github.com>
2022-03-31 18:52:45 -0400

    initial boilerplate for constant hub tracking

--- src/main/java/frc/robot/Shooter/TurretAimingPID.java
+++ src/main/java/frc/robot/Shooter/TurretAimingPID.java
@@ -1,8 +1,12 @@
 package frc.robot.Shooter;
 
 import java.util.ResourceBundle.Control;
+import java.util.function.Supplier;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
@@ -13,9 +17,11 @@ import frc.robot.Util.LimeLight.LedMode;
 
 public class TurretAimingPID extends CommandBase {
     protected LazySusanSubsystem lazySusanSubsystem;
+    protected Supplier<Pose2d> robotbase;
 
-    public TurretAimingPID(LazySusanSubsystem lazySusanSubsystem) {
+    public TurretAimingPID(LazySusanSubsystem lazySusanSubsystem, Supplier<Pose2d> robotbase) {
         this.lazySusanSubsystem = lazySusanSubsystem;
+        this.robotbase = robotbase;
         addRequirements(lazySusanSubsystem);
     }
 
@@ -26,7 +32,8 @@ public class TurretAimingPID extends CommandBase {
 
     @Override
     public void execute() {
-        double x = LimeLight.getTX();
+        double x = LimeLight.getTX(), 
+            distance = ShooterMath.getDistanceInMeters(Constants.azimuthAngle1, LimeLight.getTY(), Constants.limelightHeight, Constants.hubHeight);
         if (LimeLight.hasTarget()) {
             boolean inRange = LimeLight.inRange();
             lazySusanSubsystem.setTurretPositionDegrees(lazySusanSubsystem.getRotation().minus(Rotation2d.fromDegrees(x)));
@@ -36,8 +43,14 @@ public class TurretAimingPID extends CommandBase {
             ControlBoard.setOperatorRumble(true);
             ControlBoard.setDriverRumble(true);
         }
-        SmartDashboard.putNumber("LimeLight Distance", ShooterMath.getDistanceInMeters(Constants.azimuthAngle1, LimeLight.getTY(), Constants.limelightHeight, Constants.hubHeight));
+        SmartDashboard.putNumber("LimeLight Distance", distance);
         SmartDashboard.putNumber("LimeLight TY", LimeLight.getTY()); //TODO: Remove debug data
+
+
+        var pose = new Pose2d(this.robotbase.get().getTranslation(),  robotbase.get().getRotation().plus(lazySusanSubsystem.getRotation()));
+        var testpos = pose.transformBy(new Transform2d(new Translation2d(0, 0), Rotation2d.fromDegrees(-LimeLight.getTX()))).transformBy(
+            new Transform2d(new Translation2d(distance, 0), Rotation2d.fromDegrees(0)));
+        // asdf.robotFieldWidget.getObject("hub-target").setPose(testpos);;
     }
 
     @Override
